3cb0b6e141795568efa71b6340e7cbf69b5f6b78
Amsekal <33755443+Amsekal@users.noreply.github.com>
2020-04-03 22:36:37 +0300

    Add files via upload

---
+++ bhax-master/thematic_tutorials/bhax_textbook_IgyNeveldaProgramozod/Chomsky/hivatkozas.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{int s=0;
+for ( int i=0;i<5;i++)
+s++;
+printf("Vege\n");
+return 0;
+}
+//C99
+/*
+#include <stdio.h>
+
+int main()
+{int i,s=0;
+for ( i=0;i<5;i++)
+s++;
+printf("Vege\n");
+return 0;
+}*/
+//C98
