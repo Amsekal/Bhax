ee921d25be39968852a2a4fb90c66e845515f87a
Amsekal <33755443+Amsekal@users.noreply.github.com>
2020-10-01 14:47:10 +0200

    11 fejezet
    arroway+ törlés

--- Eltolas.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-int szo=1,db=1;
-while(szo<<=1){
-db++;}
-cout<<db<<endl;
-return 0;
-}
--- vegtelen.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-for(;;)
-cout<<"Vegtelen ciklus"<<endl;
-return 0;
-}
